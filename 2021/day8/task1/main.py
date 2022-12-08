14aa96efc35403d8aaa5e21772d6e89ac8a59955
José Romero <jose.romero@volvocars.com>
2022-12-08 13:47:30 +0100

    Added code for day8 of 2021

---
+++ 2021/day8/task1/main.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+
+TESTS_FOLDER = "tests"
+EXAMPLE_TEST_CASES = 2
+MAIN_TEST_CASES = 1
+LOG = True
+
+
+def log(msg):
+    print(msg, end="")
+
+
+def read_input(in_file: Path) -> str:
+    with open(in_file, "r") as f:
+        return f.read()
+
+
+def parse_input(in_text: str) -> list:
+    return in_text.split("\n")  # get the information
+
+
+def write_output(out_file: Path, output: str):
+    with open(out_file, "w") as f:
+        return f.write(output)
+
+def solve(in_parsed: list) -> str:
+    ans = 0
+    for info in in_parsed:
+        _, output = info.split(" | ")
+        for signal in output.split():
+            if len(signal) in (2, 3, 4, 7):  # check if the signal is a 1, 7, 4 or 8 respectively
+                ans += 1
+
+    return str(ans)
+
+
+def run_case(in_file: Path, out_file: Path):
+    in_text = read_input(in_file)
+    in_parsed = parse_input(in_text)
+    output = solve(in_parsed)
+    write_output(out_file, output)
+    return output
+
+
+def run_cases(tests_folder: Path, tests_id: list):
+    for test_id in tests_id:
+        in_file = tests_folder / f"{test_id}.in"
+        out_file = tests_folder / f"{test_id}.out"
+
+        if LOG:
+            log(f"Test case {test_id}... ")
+
+        output = run_case(in_file, out_file)
+
+        if LOG:
+            log(f"{output}\n")
+
+
+def main():
+    tests_folder = Path(__file__).parent / TESTS_FOLDER
+    tests_id = [f"{i:02}" for i in range(1, EXAMPLE_TEST_CASES + MAIN_TEST_CASES + 1)]
+
+    # Run examples test cases
+    run_cases(tests_folder, tests_id[:EXAMPLE_TEST_CASES])
+
+    # Run main test cases
+    run_cases(tests_folder, tests_id[EXAMPLE_TEST_CASES:])
+
+
+if __name__ == "__main__":
+    main()
