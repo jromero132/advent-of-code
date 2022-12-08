a2cf19f10020cfe870d310a315cfa5e56eb6e726
José Romero <jose.romero@volvocars.com>
2022-12-08 12:12:51 +0100

    Added code for day8 of 2022

---
+++ 2022/day8/task1/main.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+
+TESTS_FOLDER = "tests"
+EXAMPLE_TEST_CASES = 1
+MAIN_TEST_CASES = 1
+LOG = True
+
+
+def log(msg):
+    print(msg, end="")
+
+
+def read_input(in_file: Path) -> str:
+    with open(in_file, "r") as f:
+        return f.read()
+
+
+def parse_input(in_text: str) -> list:
+    return in_text.split("\n")  # get the grid
+
+
+def write_output(out_file: Path, output: str):
+    with open(out_file, "w") as f:
+        return f.write(output)
+
+
+def solve(in_parsed: list) -> str:
+    n, m = len(in_parsed) - 1, len(in_parsed[0]) - 1
+    visible = [  # [i][j] is True if the tree is visible, otherwise it is False
+        [
+            i == 0 or i == n or j == 0 or j == m
+            for j in range(len(in_parsed[0]))
+        ]
+        for i in range(len(in_parsed))
+    ]
+
+    for i in range(1, n):  # run for rows
+        greater = in_parsed[i][0]
+        for j in range(1, m):  # run from left to right
+            if in_parsed[i][j] > greater:
+                visible[i][j] = True
+            greater = max(greater, in_parsed[i][j])
+
+        greater = in_parsed[i][-1]
+        for j in range(m - 1, 0, -1):  # run from right to left
+            if in_parsed[i][j] > greater:
+                visible[i][j] = True
+            greater = max(greater, in_parsed[i][j])
+
+    for j in range(1, m):  # run for columns
+        greater = in_parsed[0][j]
+        for i in range(1, n):  # run from top to bottom
+            if in_parsed[i][j] > greater:
+                visible[i][j] = True
+            greater = max(greater, in_parsed[i][j])
+
+        greater = in_parsed[-1][j]
+        for i in range(n - 1, 0, -1):  # run from bottom to top
+            if in_parsed[i][j] > greater:
+                visible[i][j] = True
+            greater = max(greater, in_parsed[i][j])
+
+    return str(sum(x for row in visible for x in row))
+
+
+def run_case(in_file: Path, out_file: Path):
+    in_text = read_input(in_file)
+    in_parsed = parse_input(in_text)
+    output = solve(in_parsed)
+    write_output(out_file, output)
+    return output
+
+
+def run_cases(tests_folder: Path, tests_id: list):
+    for test_id in tests_id:
+        in_file = tests_folder / f"{test_id}.in"
+        out_file = tests_folder / f"{test_id}.out"
+
+        if LOG:
+            log(f"Test case {test_id}... ")
+
+        output = run_case(in_file, out_file)
+
+        if LOG:
+            log(f"{output}\n")
+
+
+def main():
+    tests_folder = Path(__file__).parent / TESTS_FOLDER
+    tests_id = [f"{i:02}" for i in range(1, EXAMPLE_TEST_CASES + MAIN_TEST_CASES + 1)]
+
+    # Run examples test cases
+    run_cases(tests_folder, tests_id[:EXAMPLE_TEST_CASES])
+
+    # Run main test cases
+    run_cases(tests_folder, tests_id[EXAMPLE_TEST_CASES:])
+
+
+if __name__ == "__main__":
+    main()
