5a9081f1d1f5a2b492b95163a6344fcdde2c91db
Jose A. Romero <josea132.romero@gmail.com>
2024-10-01 14:00:23 +0200

    Added code for day7 of 2022

--- 2022/day7/task2/main.py
+++ 2022/day7/task2/main.py
@@ -17,7 +17,7 @@ def read_input(in_file: Path) -> str:
 
 
 def parse_input(in_text: str) -> list:
-    return in_text.split("\n\n")  # get a summary for each elf
+    return in_text.split("\n")  # get commands
 
 
 def write_output(out_file: Path, output: str):
@@ -26,13 +26,60 @@ def write_output(out_file: Path, output: str):
 
 
 def solve(in_parsed: list) -> str:
-    # get total sum of calories carried by each elf
-    values = [sum(int(x) for x in elf.split()) for elf in in_parsed]
+    class Dir:  # dir class helper
+        def __init__(self, name: str, parent = None) -> None:
+            self.name = name
+            self.parent = parent
+            self.subdirs = {}
+            self.files = []
+            self.size = None
 
-    # sort the elves decreasingly by carrying amount
-    values = sorted(values, reverse=True)
+    dir_tree = Dir("/")  # root dir
+    cwd = dir_tree
+    for cmd in in_parsed:
+        cmd = cmd.split()
+        match cmd[0]:
+            case "$":
+                if cmd[1] == "cd":
+                    match cmd[2]:
+                        case "/":  # go to root directory
+                            cwd = dir_tree
 
-    return str(sum(values[:3]))  # sum the top 3 elves carrying the most calories
+                        case "..":  # go to parent directory
+                            cwd = cwd.parent
+
+                        case _:  # go to subdirectory
+                            cwd = cwd.subdirs[cmd[2]]
+
+            case "dir":  # current directory has a 'cmd[2]' subdirectory
+                cwd.subdirs[cmd[1]] = Dir(cmd[1], cwd)
+
+            case _:  # current directory has a 'cmd[1]' file with 'cmd[0]' size
+                cwd.files.append((cmd[1], int(cmd[0])))
+
+    def update_size(cwd):  # update directory size
+        cwd.size = 0
+        for d in cwd.subdirs.values():
+            update_size(d)  # recursively work on subdirectories
+            cwd.size += d.size
+
+        for _, fs in cwd.files:  # work on files
+            cwd.size += fs
+
+    # get the directory with the smallest size such that after delete it,
+    # 0the amount of free space is greater than the required space
+    def get_directory_to_delete(cwd, free_space, required_space):
+        ans = cwd
+        for d in cwd.subdirs.values():
+            if free_space + d.size >= required_space:
+                sub_ans = get_directory_to_delete(d, free_space, required_space)
+                if ans.size > sub_ans.size:
+                    ans = sub_ans
+        return ans
+
+    update_size(dir_tree)  # update directory sizes
+    folder = get_directory_to_delete(dir_tree, 70000000 - dir_tree.size, 30000000)  # get the folder to be deleted
+    return str(folder.size)  # get the size of the folder to be deleted
 
 
 def run_case(in_file: Path, out_file: Path):
