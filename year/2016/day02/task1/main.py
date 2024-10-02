dc5689e6595c684c1bc7fa8086b3decc7a8c03b7
Jose A. Romero <josea132.romero@gmail.com>
2024-10-02 17:12:46 +0200

    Add code for day2 of 2016

---
+++ year/2016/day02/task1/main.py
@@ -0,0 +1,36 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2016 ; day=2 ; task=1)
+"""
+
+import sys
+
+
+def main():
+    code = ""
+    r, c = 1, 1
+    for line in sys.stdin:
+        for l in line:
+            match l:
+                case "U":
+                    r = max(0, r - 1)
+
+                case "R":
+                    c = min(2, c + 1)
+
+                case "D":
+                    r = min(2, r + 1)
+
+                case "L":
+                    c = max(0, c - 1)
+
+                case _:  # Error
+                    pass
+
+        code += str(r * 3 + c + 1)
+
+    print(code)
+
+
+if __name__ == "__main__":
+    main()
---
+++ year/2016/day02/task2/main.py
@@ -0,0 +1,47 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2016 ; day=2 ; task=2)
+"""
+
+import sys
+
+
+def main():
+    code = ""
+    grid = (
+        ("", "", "1", "", "", ),
+        ("", "2", "3", "4", "",),
+        ("5", "6", "7", "8", "9",),
+        ("", "A", "B", "C", "",),
+        ("", "", "D", "", "",),
+    )
+    r, c = 2, 0
+    for line in sys.stdin:
+        for l in line:
+            nr, nc = r, c
+            match l:
+                case "U":
+                    nr -= 1
+
+                case "R":
+                    nc += 1
+
+                case "D":
+                    nr += 1
+
+                case "L":
+                    nc -= 1
+
+                case _:  # Error
+                    pass
+
+            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc]:
+                r, c = nr, nc
+
+        code += grid[r][c]
+
+    print(code)
+
+
+if __name__ == "__main__":
+    main()
