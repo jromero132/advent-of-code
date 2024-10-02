0ba2b51f96d281319c28529adae2f3db304ac702
Jose A. Romero <josea132.romero@gmail.com>
2024-10-02 17:12:46 +0200

    Add code for day2 of 2015

--- year/2015/day02/task1/main.py
+++ year/2015/day02/task1/main.py
@@ -3,9 +3,17 @@ Author: Jose A. Romero
 Puzzle: Advent of Code (year=2015 ; day=2 ; task=1)
 """
 
+import sys
+
 
 def main():
-    line = input()
+    ans = 0
+    for line in sys.stdin:
+        dims = [int(n) for n in line.split("x")]
+        faces = [dims[i] * dims[j] for i in range(len(dims)) for j in range(i + 1, len(dims))]
+        ans += 2 * sum(faces) + min(faces)
+
+    print(ans)
 
 
 if __name__ == "__main__":
---
+++ year/2015/day02/task2/main.py
@@ -0,0 +1,24 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2015 ; day=2 ; task=2)
+"""
+
+import sys
+
+
+def main():
+    ans = 0
+    for line in sys.stdin:
+        dims = [int(n) for n in line.split("x")]
+        ribbon, bow, m = 0, 1, 0
+        for d in dims:
+            m = max(m, d)
+            ribbon += d
+            bow *= d
+
+        ans += 2 * (ribbon - m) + bow
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
