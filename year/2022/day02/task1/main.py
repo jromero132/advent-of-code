a1e3087bc980f791ca17a6b70ba64a3a39f54b6c
Jose A. Romero <josea132.romero@gmail.com>
2024-10-03 10:18:15 +0200

    Add code for day2 of 2022

--- year/2022/day02/task1/main.py
+++ year/2022/day02/task1/main.py
@@ -9,9 +9,9 @@ import sys
 def main():
     ans = 0
     for line in sys.stdin:
-        p1 = ord(line[0]) - ord("A")
-        p2 = ord(line[2]) - ord("X")
-        result = (3 + p1 - p2) % 3
+        p1 = ord(line[0]) - ord("A")  # 0=rock ; 1=paper ; 2=scissors
+        p2 = ord(line[2]) - ord("X")  # 0=rock ; 1=paper ; 2=scissors
+        result = (3 + p1 - p2) % 3  # 0=tie ; 1=p1 wins ; 2=p2 wins
         ans += p2 + 1 + 3 * ((1 - result) % 3)
 
     print(ans)
--- year/2022/day02/task2/main.py
+++ year/2022/day02/task2/main.py
@@ -9,10 +9,10 @@ import sys
 def main():
     ans = 0
     for line in sys.stdin:
-        p1 = ord(line[0]) - ord("A")
-        result = (1 - ord(line[2]) + ord("X")) % 3
-        p2 = (3 + p1 - result) % 3
-        ans += p2 + 1 + (3, 0, 6)[result]
+        p1 = ord(line[0]) - ord("A")  # 0=rock ; 1=paper ; 2=scissors
+        result = (1 - ord(line[2]) + ord("X")) % 3  # 0=tie ; 1=p1 wins ; 2=p2 wins
+        p2 = (3 + p1 - result) % 3  # 0=rock ; 1=paper ; 2=scissors
+        ans += p2 + 1 + 3 * ((1 - result) % 3)
 
     print(ans)
 
