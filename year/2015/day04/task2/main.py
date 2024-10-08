d1b39759f4c5305046bf2ca8687b7f4c1f03dbab
Jose A. Romero <josea132.romero@gmail.com>
2024-10-08 12:10:35 +0200

    Add code for day4 of 2015

---
+++ year/2015/day04/task2/main.py
@@ -0,0 +1,21 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2015 ; day=4 ; task=2)
+"""
+
+import hashlib
+import sys
+
+
+def main():
+    secret_key = sys.stdin.readline().strip()
+    n, hash = 0, ""
+    while not hash.startswith("000000"):
+        n += 1
+        hash = hashlib.md5(f"{secret_key}{n}".encode()).hexdigest()
+
+    print(n)
+
+
+if __name__ == "__main__":
+    main()
