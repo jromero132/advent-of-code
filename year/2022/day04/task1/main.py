1a21a31f824c2e26230a548528e24e619b7b5b4d
Jose A. Romero <josea132.romero@gmail.com>
2024-10-08 21:26:10 +0200

    Add code for day4 of 2022

---
+++ year/2022/day04/task1/main.py
@@ -0,0 +1,22 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2022 ; day=4 ; task=1)
+"""
+
+import sys
+
+
+def main():
+    ans = 0
+    for s1, s2 in (line.split(",") for line in sys.stdin):
+        r1 = [int(x) for x in s1.split("-")]
+        r2 = [int(x) for x in s2.split("-")]
+
+        # check if one section fully overlaps the other
+        ans += (r1[0] <= r2[0] and r2[1] <= r1[1]) or (r2[0] <= r1[0] and r1[1] <= r2[1])
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
