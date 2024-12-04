0b41f6c3e41cee92ac0b78eb98e1c3a8a29acadc
Jose A. Romero <josea132.romero@gmail.com>
2024-12-04 13:40:25 +0100

    Add code for day 6 of 2016

---
+++ year/2016/day06/task2/main.py
@@ -0,0 +1,17 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2016 ; day=6 ; task=2)
+"""
+
+import sys
+from collections import Counter
+
+
+def main() -> None:
+    grid = list(sys.stdin)
+    counter = [Counter(grid[i][j] for i in range(len(grid))) for j in range(len(grid[0]))]
+    print("".join(c.most_common()[-1][0] for c in counter))
+
+
+if __name__ == "__main__":
+    main()
