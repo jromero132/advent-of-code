b7003892db78ce8ab7af8a256398774e9e87e90f
Jose A. Romero <josea132.romero@gmail.com>
2024-12-07 13:37:54 +0100

    Add code for day 6 of 2022

---
+++ year/2022/day06/task2/main.py
@@ -0,0 +1,25 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2022 ; day=6 ; task=2)
+"""
+
+import sys
+
+
+def main() -> None:
+    line = sys.stdin.read().strip()
+    marker_size = 14
+    print(
+        next(
+            (
+                str(i)
+                for i in range(marker_size, len(line) + 1)
+                if len(set(line[i - marker_size : i])) == marker_size
+            ),
+            "-1",
+        ),
+    )
+
+
+if __name__ == "__main__":
+    main()
