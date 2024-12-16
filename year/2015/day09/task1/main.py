768a04db429bc6bcaf7f6474153c26c82ce956ad
Jose A. Romero <josea132.romero@gmail.com>
2024-12-16 02:48:04 +0100

    Add code for day 9 of 2015

---
+++ year/2015/day09/task1/main.py
@@ -0,0 +1,30 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2015 ; day=9 ; task=1)
+"""
+
+import sys
+from collections import defaultdict
+from itertools import permutations
+
+
+def main() -> None:
+    edges = [line.strip().split() for line in sys.stdin]
+    edges = [(line[0], line[2], int(line[4])) for line in edges]
+    graph = defaultdict(dict)
+    ans = 0
+    for u, v, w in edges:
+        graph[u][v] = w
+        graph[v][u] = w
+        ans += w
+
+    # This is the Traveling Salesman Problem
+    print(
+        min(
+            sum(graph[u][v] for u, v in zip(path, path[1:])) for path in permutations(graph.keys())
+        ),
+    )
+
+
+if __name__ == "__main__":
+    main()
