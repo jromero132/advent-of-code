8a223cd2546f80abd075e0e2895161039bea32bb
Jose A. Romero <josea132.romero@gmail.com>
2025-01-11 20:54:12 +0100

    Add code for day 10 of 2015

---
+++ year/2015/day10/task1/main.py
@@ -0,0 +1,27 @@
+"""
+Author: Jose A. Romero
+Puzzle: Advent of Code (year=2015 ; day=10 ; task=1)
+"""
+
+import sys
+
+
+def main() -> None:
+    line = sys.stdin.read().strip()
+    for _ in range(40):
+        ans, cnt = "", 1
+        for i in range(1, len(line)):
+            if line[i] == line[i - 1]:
+                cnt += 1
+
+            else:
+                ans += str(cnt) + line[i - 1]
+                cnt = 1
+
+        ans += str(cnt) + line[-1]
+        line = ans
+    print(len(ans))
+
+
+if __name__ == "__main__":
+    main()
