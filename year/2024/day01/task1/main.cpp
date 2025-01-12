42369cd1b74e18f989a8889ca37d7f2951f8ce38
Jose A. Romero <josea132.romero@gmail.com>
2025-01-12 08:55:13 +0100

    Add code for day 1 of 2024 in C++

---
+++ year/2024/day01/task1/main.cpp
@@ -0,0 +1,28 @@
+/*
+Author : Jose A.Romero
+Puzzle : Advent of Code(year=2024 ; day=1 ; task=1)
+*/
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int a, b;
+    vector<int> group1, group2;
+    while (cin >> a >> b) {
+        group1.push_back(a);
+        group2.push_back(b);
+    }
+    sort(group1.begin(), group1.end());
+    sort(group2.begin(), group2.end());
+    int ans = 0;
+    for (int i = 0; i < group1.size(); ++i) {
+        ans += abs(group1[i] - group2[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ year/2024/day01/task2/main.cpp
@@ -0,0 +1,26 @@
+/*
+Author : Jose A.Romero
+Puzzle : Advent of Code(year=2024 ; day=1 ; task=2)
+*/
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int a, b;
+    vector<int> group1, group2;
+    while (cin >> a >> b) {
+        group1.push_back(a);
+        group2.push_back(b);
+    }
+    int ans = 0;
+    for (int x : group1) {
+        ans += x * count(group2.begin(), group2.end(), x);
+    }
+    cout << ans << endl;
+    return 0;
+}
